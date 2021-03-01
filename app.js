d414acecc277e97b8a400b2c9df32958be0d11db
BlasToth <balazstothlaszlo+github@gmail.com>
2021-03-01 21:12:59 +0100

    add some style

--- app.js
+++ app.js
@@ -4,20 +4,27 @@ const divLogin = document.querySelector(".login");
 const divRegister = document.querySelector(".register");
 
 const showLogin = () => {
-divLogin.classList.add('visible');
-divRegister.classList.remove('visible');
-// btnLogin.classList.add('invisible');
+    divLogin.classList.add('visible');
+    // btnLogin.classList.add('visible');
+    divRegister.classList.add('invisible');
+    // btnRegister.classList.add('invisible');
+    divRegister.classList.remove('visible');
+    // btnRegister.classList.remove('visible');
+    divLogin.classList.remove('invisible');
+    // btnLogin.classList.remove('invisible');
 };
 
 const showRegister = () => {
     divRegister.classList.add('visible');
+    // btnRegister.classList.add('visible');
+    divLogin.classList.add('invisible');
+    // btnLogin.classList.add('invisible');
     divLogin.classList.remove('visible');
-    // btnRegister.classList.add('invisible');
+    // btnLogin.classList.remove('visible');
+    divRegister.classList.remove('invisible');
+    // btnRegister.classList.remove('invisible');
 };
 
 
 btnLogin.addEventListener('click', showLogin);
 btnRegister.addEventListener('click', showRegister);
-// btnRegister.addEventListener('click', () => {
-//     divRegister.classList.add('visible');
-// })
