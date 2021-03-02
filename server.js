c35d7641b5b428316d9504a259767273f052bbd7
BlasToth <balazstothlaszlo+github@gmail.com>
2021-03-02 19:36:58 +0100

    link style and js with index.ejs

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+// Imports
 const express = require('express');
 const app = express();
+const port = 3000;
 
+// Static files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+
+// Set Templating Engine
 app.set('view-engine', 'ejs');
 
+// Navigation
 app.get('/', (req, res) => {
     res.render('index.ejs', { name: 'JonNieve' })
 })
 
-app.listen(3000);
+// Listen on port 3000
+app.listen(port, () => console.info(`App listening on port ${port}`));
